3793a380a430d0167cbf9cd91fef415580e0f3b5
KHJ_940803 <adsf465@ajou.ac.kr>
2021-12-08 23:38:00 +0900

    211208.py는 naver_count.csv를 만든다. movieset.csv는 영화진흥위에서 가져왔다. spark.py를 통해 part-000000.....   .csv 파일을 만든다. ./bin/spark-submit spark.py 를 해야 결과를 만들 수 있다. spark.apache.org/downloads.html 에서 spark-3.2.x-bin-hadoop2.7.tgz 를 받고 압축을 해제해서
    cd $SPARK_HOME/conf
    
    cp spark-defaults.conf.template spark-defaults.conf
    cp spark-env.sh.template spark-env.sh
    cp log4j.properties.template log4j.properties
    
    vi log4j.properties
    
    log4j.rootCategory=WARN, console
    
    하고
    
    cd $SPARK_HOME/
    ./bin/spark-submit spark.py
    한다

---
+++ spark.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType 
+from pyspark.sql.types import ArrayType, DoubleType, BooleanType
+from pyspark.sql.functions import col,array_contains
+from pyspark.sql import SQLContext
+import time
+start = time.time()
+
+
+conf = SparkConf().setMaster("local").setAppName("spark")
+sc = SparkContext(conf=conf) # spark context를 파이썬에서 사용하기 위함이다.
+sqlContext = SQLContext(sc) # spark sqlContext를 파이썬에서 사용하기 위함이다.
+
+# spark를 파이썬에서 사용하기 위함이다.
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("spark") \
+    .getOrCreate()
+
+list = spark.read.option("header", "true").csv("./movielist.csv") # 영화 목록 csv를 불러온다.
+list.show(list.count())
+naverCount = spark.read.option("header", "true").csv("./naver_count.csv") # 크롤링의 결과를 카운트한 csv를 불러온다.
+naverCount.show(naverCount.count())
+rdd = list.join(naverCount, list["Title"] == naverCount["Name"],"inner").drop("Name").rdd #두개의 dataframe을 합치고 rdd로 만든다.
+df = sqlContext.createDataFrame(rdd, ['Title', 'Genre','Director','Count']) # rdd를 dataframe으로 만든다.
+df.coalesce(1).write.format('com.databricks.spark.csv').options(header='true').save('./result')# result directory make
+print("time :", time.time() - start) 
